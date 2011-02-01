3591083f872bbe762833fdd9166f9f6549501089
Norio Nomura <norio.nomura@gmail.com>
2011-02-01 11:24:29 +0900

    fix error causing on Safari.

--- SendToAirFlick.safariextension/SendToAirFlick.js
+++ SendToAirFlick.safariextension/SendToAirFlick.js
@@ -51,11 +51,13 @@ if (window.top === window) {
         a.addEventListener('mouseout', function() {a.style.color = 'transparent';}, false);
         window.setTimeout(function(){try{a.style.color = 'transparent';}catch(e){};}, 5000);
       } else if (maybePicture.test(event.url)) {
-        event.target.addEventListener("contextmenu", function (event) {
-          safari.self.tab.setContextMenuEventUserInfo(event, {
-            nodeName: event.target.nodeName, srcUrl: event.target.src
-          });
-        }, true);
+        if (typeof(safari) !== 'undefined') {
+          event.target.addEventListener("contextmenu", function (event) {
+            safari.self.tab.setContextMenuEventUserInfo(event, {
+              nodeName: event.target.nodeName, srcUrl: event.target.src
+            });
+          }, true);
+        }
       }
     }
   }, true);
